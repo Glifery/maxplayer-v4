90aa684be05337164a47f9b9568075f4e350565b
Glifery <glifery@gmail.com>
2017-10-18 22:56:03 +0300

    Delete .build folder from git

--- backend/google-music/.build/handler.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TypescriptHandler_1 = require("./handlers/TypescriptHandler");
-const typescript = (event, context, callback) => (new TypescriptHandler_1.TypescriptHandler).handle(event, context, callback);
-exports.typescript = typescript;
